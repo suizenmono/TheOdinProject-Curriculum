ec2e4ae198331fa8652c9a1c98d01052d51c86bf
suizenmono <190133959+suizenmono@users.noreply.github.com>
2025-07-12 05:15:55 +0200

    Implement randomization of opponent's move

---
+++ projects/rock-paper-scissors/index.js
@@ -0,0 +1,19 @@
+function drawComputerMove() {
+    let rng = Math.random() * 3; // yields rng in [0,3)
+    let computerMove;
+    
+    // excluded upper boundary means intervals are uniform => no bias
+    if (rng >= 0 && rng < 1) {
+        computerMove = "Rock";
+    }
+    else if (rng >= 1 && rng < 2) {
+        computerMove = "Paper";
+    }
+    else { // rng >= 2 && rng < 3
+        computerMove = "Scissors";
+    }
+
+    // console.log(rng, computerMove);
+    return computerMove;
+}
+
